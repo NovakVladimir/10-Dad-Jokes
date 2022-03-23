a657a1b2763f3e5306d0b4bec4441e64b315c457
NovakVladimir <vladimir.novak.89@i.ua>
2022-03-23 22:52:35 +0200

    Add audio

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
-const jokeEl = document.getElementById('joke');
-const jokeBtn = document.getElementById('jokeBtn');
+const word = document.getElementById('yourText');
+const audio = document.getElementById('aud');
+const speak = document.getElementById('speak');
 
-jokeBtn.addEventListener('click', generateJoke);
 
-generateJoke();
 
-async function generateJoke() {
-    const config =  {
-        headers: {
-        Accept: 'application/json'
-        }
-    }
-    const res = await fetch('https://icanhazdadjoke.com', config);
-    
+audio.addEventListener('click', voice);
+
+
+async function voice() {
+    const au = document.getElementById('startMusic');
+    let adress = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value;
+    const res = await fetch(adress);
     const data = await res.json();
-
-    jokeEl.innerHTML = data.joke;
+    au.src = await data[0].phonetics[0].audio;
+    au.play();
+    speak.innerHTML = data[0].phonetic===undefined? "-" : data[0].phonetic;
 }
 
-// function generateJoke() {
-//     const config =  {
-//         headers: {
-//         Accept: 'application/json'
-//         }
-//     }
-//     fetch('https://icanhazdadjoke.com', config)
-//     .then(res => res.json())
-//     .then(data => {
-//         jokeEl.innerHTML = data.joke;
-//     });
-// }
+
+
 
 
 
