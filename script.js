2eb6dc4461b76f2ac4af7402e60904aae3a079cd
NovakVladimir <vladimir.novak.89@i.ua>
2022-03-24 08:25:25 +0200

    add HTML, CSS, JS

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const word = document.getElementById('yourText');
 const audio = document.getElementById('aud');
 const speak = document.getElementById('speak');
+const mean = document.getElementById('mean');
 
 
 
@@ -8,6 +9,7 @@ audio.addEventListener('click', voice);
 
 
 async function voice() {
+    
     const au = document.getElementById('startMusic');
     let adress = 'https://api.dictionaryapi.dev/api/v2/entries/en/' + word.value;
     const res = await fetch(adress);
@@ -15,6 +17,7 @@ async function voice() {
     au.src = await data[0].phonetics[0].audio;
     au.play();
     speak.innerHTML = data[0].phonetic===undefined? "-" : data[0].phonetic;
+    // mean.innerHTML = data[]
 }
 
 
